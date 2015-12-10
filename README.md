454293ef6c1441ac2024d724e48d64ffc4fcc332
Oliver Sierra <oliversierrab@gmail.com>
2015-12-10 00:07:15 -0800

    Creating basic configuration.

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # responsive-images
 Responsive image generator.
+
+1) 	## INSTALL [vips](http://www.vips.ecs.soton.ac.uk/index.php?title=Build_on_OS_X)
+	```shell
+	brew install vips --with-webp --with-graphicsmagick --without-check
+	```
+	* Go for a coffee...
+
+2)	npm install
+
+Dependencies (included in package.json)
+* [sharp](http://sharp.dimens.io/en/stable/install/)	
+* [gulp-responsive](https://github.com/mahnunchik/gulp-responsive)
+
+Other Dependencies
+* 	[gulp](https://www.npmjs.com/package/gulp)
+	npm --save-dev install gulp -g 
+
+# ISSUES INSTALLING VIPS?
+Try:
+
+1) brew doctor
+2) brew update
+3) Wiwi: https://github.com/Homebrew/homebrew/blob/master/share/doc/homebrew/Troubleshooting.md#troubleshooting
---
+++ gulpfile.js
@@ -0,0 +1,93 @@
+var gulp = require('gulp');
+var clean = require('gulp-clean');
+var responsive = require('gulp-responsive');
+var config = require('./config.json');
+
+gulp.task('clean', function () {
+    return gulp.src(config.paths.destination, {read: false})
+        .pipe(clean());
+});
+
+gulp.task('generate', ['clean'], function () {
+    return gulp.src(config.paths.source)
+    .pipe(responsive([{
+        name: '**/*.jpg',
+        width: config.sizes.small,
+        rename: {
+          suffix: config.suffix.small
+        }
+    },{
+        name: '**/*.jpg',
+        width: config.sizes.small2x,
+        rename: {
+          suffix: config.suffix.small2x
+        }
+    },{
+        name: '**/*.jpg',
+        width: config.sizes.medium,
+        rename: {
+          suffix: config.suffix.medium
+        }
+    },{
+        name: '**/*.jpg',
+        width: config.sizes.medium2x,
+        rename: {
+          suffix: config.suffix.medium2x
+        }
+    },{
+        name: '**/*.jpg',
+        width: config.sizes.large,
+        rename: {
+          suffix:config.suffix.large
+        }
+    },{
+        name: '**/*.jpg',
+        width: config.sizes.large2x,
+        rename: {
+          suffix: config.suffix.large2x
+        }
+    },{
+        name: '**/*.png',
+        width: config.sizes.small,
+        rename: {
+          suffix: config.suffix.small
+        }
+    },{
+        name: '**/*.png',
+        width: config.sizes.small2x,
+        rename: {
+          suffix: config.suffix.small2x
+        }
+    },{
+        name: '**/*.png',
+        width: config.sizes.medium,
+        rename: {
+          suffix: config.suffix.medium
+        }
+    },{
+        name: '**/*.png',
+        width: config.sizes.medium2x,
+        rename: {
+          suffix: config.suffix.medium2x
+        }
+    },{
+        name: '**/*.png',
+        width: config.sizes.large,
+        rename: {
+          suffix:config.suffix.large
+        }
+    },{
+        name: '**/*.png',
+        width: config.sizes.large2x,
+        rename: {
+          suffix: config.suffix.large2x
+        }
+    }],{
+      quality: 80,
+      // errorOnUnusedImage, false,
+      compressionLevel: 9 //Default 6
+    }))
+    .pipe(gulp.dest(config.paths.destination));
+});
+
+gulp.task('build', ['generate']);
